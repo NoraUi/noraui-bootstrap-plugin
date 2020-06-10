8ed6fc90a343302c157f71c182047cd544c75de8
sgrillon14 <sgrillon@hotmail.fr>
2020-06-10 22:21:11 +0200

    initial commit

---
+++ src/main/java/com/github/noraui/application/steps/themes/bootstrap/BootstrapSteps.java
@@ -0,0 +1,19 @@
+/**
+ * NoraUi is licensed under the license GNU AFFERO GENERAL PUBLIC LICENSE
+ *
+ * @author Nicolas HALLOUIN
+ * @author Stéphane GRILLON
+ */
+package com.github.noraui.application.steps.themes.bootstrap;
+
+import org.slf4j.Logger;
+
+import com.github.noraui.application.steps.Step;
+import com.github.noraui.log.annotation.Loggable;
+
+@Loggable
+public class BootstrapSteps extends Step {
+
+    static Logger log;
+
+}
